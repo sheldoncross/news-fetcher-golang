addcddc66b305e3fb680b8881be48517888c3e36
Stefan Cross <sheldoncrosss@gmail.com>
2024-06-04 01:30:18 -0400

    AI generated headlines

--- news.go
+++ news.go
@@ -1,23 +1,36 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/google/generative-ai-go/genai"
+	"github.com/joho/godotenv"
+	"google.golang.org/api/option"
+	"github.com/hegedustibor/htgo-tts"
+	"github.com/hegedustibor/htgo-tts/voices"
 )
 
 type NewsApiResponse struct {
 	Articles []struct {
-		Title			string `json:"title"`
-		Description		string `json:"description"`
-		URL				string `json:"url"`
+		Title       string `json:"title"`
+		Description string `json:"description"`
+		URL         string `json:"url"`
 	} `json:"articles"`
 }
 
 func main() {
-	apiKey := "b2654a987dde4d0ba228c0c44e5141f6"
-	endpoint := fmt.Sprintf("https://newsapi.org/v2/top-headlines?country=us&apiKey=%s", apiKey)
+	gemini()
+}
+
+func getHeadlines() []string {
+	news_apiKey := os.Getenv("NEWS_API_KEY")
+	endpoint := fmt.Sprintf("https://newsapi.org/v2/top-headlines?country=ca&apiKey=%s", news_apiKey)
 
 	resp, err := http.Get(endpoint)
 	if err != nil {
@@ -31,11 +44,62 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Fetching News\n")
+	fmt.Printf("Fetching News\n\n")
+
+	var titles []string
 
 	for _, article := range newsData.Articles {
-		fmt.Printf("Title: %s\n", article.Title)
-		fmt.Printf("Description: %s\n", article.Description)
-		fmt.Printf("URL: %s\n\n", article.URL)
+		titles = append(titles, article.Title)
+	}
+
+	return titles
+}
+
+func gemini() {
+	//Load key
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .enf file", err)
+	}
+	gemini_apiKey := os.Getenv("GEMINI_API_KEY")
+
+	//Create context and init
+	context := context.Background()
+	client, err := genai.NewClient(context, option.WithAPIKey(gemini_apiKey))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	titles := getHeadlines()
+
+	model := client.GenerativeModel("gemini-1.5-pro")
+	cs := model.StartChat()
+
+	send := func(msg string) *genai.GenerateContentResponse {
+		res, err := cs.SendMessage(context, genai.Text(msg))
+		if err != nil {
+			log.Fatal(err)
+		}
+		return res
+	}
+
+	for _, title := range titles {
+		if len(title) > 25 {
+			fmt.Printf("%s\n\n", title)
+			data := fmt.Sprintf("Read this headline in the voice of a newscaster, but keep it short. %s", title)
+			res := send(data)
+			for _, candidate := range res.Candidates {
+				if candidate.Content != nil {
+					switch part := candidate.Content.Parts[0].(type) {
+					case genai.Text:
+						speech := htgotts.Speech{Folder: "audio", Language: voices.English}
+						fmt.Printf(string(part))
+						speech.Speak(string(part))
+					}
+				}
+			}
+			time.Sleep(2 * time.Second)
+		}
 	}
 }
